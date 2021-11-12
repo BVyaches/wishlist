10edd9e6b3bbaa48d20769cf99fbdd9a9c208d43
Firesgame <74917093+Firesgame@users.noreply.github.com>
2021-11-12 14:30:42 +0500

    Update settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -97,7 +97,8 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
